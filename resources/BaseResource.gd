6141c839df88dec169882dccca722533ab137325
cai49 <75114312+cai49@users.noreply.github.com>
2024-03-22 02:31:20 -0700

    Finished First Iteration

---
+++ resources/BaseResource.gd
@@ -0,0 +1,9 @@
+class_name BaseResource extends Resource
+
+@export_category("Identification")
+@export var id: String
+@export var name: String
+
+@export_category("Values")
+@export var rarity: int
+@export_multiline var description: String
--- tool_scripts/CDBReader.gd
+++ tool_scripts/CDBReader.gd
@@ -1,13 +1,29 @@
-
+@tool
 extends EditorScript
 
-const FILE: String = "res://resources/testDB.cdb"
+const FILE: String = "res://static_data/testDB.cdb"
+const RES_FOLDER: String = "resources/"
 
 func _run() -> void:
 	var file = FileAccess.open(FILE, FileAccess.READ)
 	if file:
 		var dict: Dictionary = JSON.parse_string(file.get_as_text()) as Dictionary
 		var resources: Array = dict.get("sheets")[0].get("lines") as Array
+		
 		for resource in resources:
-			print(resource)
+			var new_res: BaseResource = BaseResource.new()
+			new_res.id = resource.get("id")
+			new_res.name = resource.get("name")
+			new_res.rarity = int(resource.get("rarity"))
+			new_res.description = resource.get("description")
+			
+			var res_name: String = '%s%s%s%s' % ['res://', RES_FOLDER, new_res.name, '.tres']
+			
+			# Have not found a way to make the updated resources to show until restarting editor
+			if ResourceLoader.exists(res_name):
+				EditorInterface.restart_editor(true)
+
+			var saveRes: Error = ResourceSaver.save(new_res, res_name)
+			if saveRes != OK:
+				print(error_string(saveRes))
 			
